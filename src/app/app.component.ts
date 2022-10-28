6093377e9a411aaaf1734d95bcf8184afc1ffacc
jorivera <jorivera@comasw.com>
2022-10-28 11:40:39 -0500

    Creacion de proyecto, Primeros pasos

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'pipesApp';
+  nombre:string = 'joSé LuIS';
+  valor:number = 200000000;
+  MostrarNombre(){
+   console.log(this.nombre);
+  }
+}
