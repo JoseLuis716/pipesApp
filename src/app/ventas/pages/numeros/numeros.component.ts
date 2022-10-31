094a327f9948ad5f186a6bbddc3bed5b730ad97e
jorivera <jorivera@comasw.com>
2022-10-31 17:51:06 -0600

    Pipes de porcentaje y decimales.

--- src/app/ventas/pages/numeros/numeros.component.ts
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numeros',
@@ -6,11 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NumerosComponent implements OnInit {
+export class NumerosComponent {
 
+  dVentasNetas:number = 2779134850.5529;
+  dPorcentaje:number = 0.4857;  
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }
