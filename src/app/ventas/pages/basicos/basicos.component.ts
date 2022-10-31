fbb76be3c74acf42a1c2f9c3521da3535f6a705b
jorivera <jorivera@comasw.com>
2022-10-31 15:31:55 -0600

    Pipes con texto

--- src/app/ventas/pages/basicos/basicos.component.ts
+++ src/app/ventas/pages/basicos/basicos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-basicos',
@@ -6,11 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicosComponent implements OnInit {
-
+export class BasicosComponent {
+  cDefinicionUpper: string = "convierte texto a mayúsculas";
+  cDefinicionLower: string = "CONVIERTE TEXTO A MINUSCULAS";
+  cDefinicionTittle: string = "caPitAlizA eL tExTo  ";
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
