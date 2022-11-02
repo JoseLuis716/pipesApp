7feda084881dfc5a437520c6e5f3a1c10dad3287
jorivera <jorivera@comasw.com>
2022-11-02 11:16:39 -0600

    slice

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -26,6 +27,51 @@ export class NoComunesComponent {
     'other': 'tenemos # clientes esperando'
   }
   constructor() { }
- 
+  
+  CambiarGenero(){
+    if(this.cGenero=='masculino'){
+      //cambiar a fenemino
+      this.cNombre = "Lizbeth";
+      this.cGenero = "femenino";
+    }else{
+      this.cNombre = "José Luis";
+      this.cGenero = "masculino";
+    }
+  }
 
+  RestarCliente(){
+    this.clientes.pop();
+  }
+
+  //keyValuePipe
+  persona = {
+    nombre: "Jose Luis",
+    edad: 25,
+    direccion: "Mérida, Yuc"
+  }
+
+  //JSONPipe
+  heroes = [
+    {
+      nombre:"Super Man",
+      vuela: "true",
+    },
+    {
+      nombre:"Spider Man",
+      vuela: "false",
+    },
+    {
+      nombre:"Aquaman Man",
+      vuela: "false",
+    }
+  ];
+
+  //asyncPipe
+  miObservable = interval (100); //0,1,2,3,4
+
+  valorPromesa = new Promise((resolve,reject) =>{
+    setTimeout(() => {
+      resolve('Tenemos data de promesas')
+    }, 3500);
+  });
 }
